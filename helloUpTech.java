965352de153b82010fa2c1c8b3e20c68accd0482
montassar14 <74435513+montassar14@users.noreply.github.com>
2021-07-02 06:49:04 +0100

    adding 2nd line

--- helloUpTech.java
+++ helloUpTech.java
@@ -3,7 +3,7 @@ public class helloUpTech {
       public static void main (String[] args) {
       
            System.out.println("hello UpTech");
-           
+           System.out.println("I'm montassar sghaier");
            }
            
            }
